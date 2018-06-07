e440e9d346d14d7cb12f465005d43e686c654057
Anthony Miyoro Omagwa <92746@std.strathmore.local>
2018-06-07 16:09:17 +0300

    Working user login

--- src/main/java/am/moviepredictorai/MvcConfig.java
+++ src/main/java/am/moviepredictorai/MvcConfig.java
@@ -1,12 +1,16 @@
 package am.moviepredictorai;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Controller
+@ComponentScan
 public class MvcConfig implements WebMvcConfigurer{
     public void addViewControllers(ViewControllerRegistry registry){
         registry.addViewController("/home").setViewName("home");
-        registry.addViewController("/").setViewName("home");
+//        registry.addViewController("/").setViewName("home");
         registry.addViewController("/hello").setViewName("hello");
         registry.addViewController("/login").setViewName("login");
     }
