0a55168496def4432744132620a2b98ebab8ab1a
Anthony Miyoro <amiyoro@gmail.com>
2018-06-25 11:06:16 +0300

    not workibg javax

--- src/main/java/am/moviepredictorai/Application.java
+++ src/main/java/am/moviepredictorai/Application.java
@@ -6,9 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 //Run the application java class
 @SpringBootApplication
-@EnableOAuth2Sso
+//@EnableOAuth2Sso
 public class Application {
-
     public static void main(String[] args){
         SpringApplication.run(Application.class, args);
     }
